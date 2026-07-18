14d7b90779e0b37b2b36993cc2d6c1dfc8190dc5
Khadeeja Memon <memon12787@gmail.com>
2026-07-18 16:04:04 +0500

    Prepare backend for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -71,18 +71,18 @@ app.get("/", (req,res)=>{
 
 
 
-// Local Development Only
-if(process.env.NODE_ENV !== "production"){
+// // Local Development Only
+// if(process.env.NODE_ENV !== "production"){
 
-  const PORT = process.env.PORT || 5000;
+//   const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT,()=>{
-    console.log(
-      `Server running on port ${PORT}`
-    );
-  });
+//   app.listen(PORT,()=>{
+//     console.log(
+//       `Server running on port ${PORT}`
+//     );
+//   });
 
-}
+// }
 
 
 // Vercel serverless export
